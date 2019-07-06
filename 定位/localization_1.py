5fea09a9537326ea2285e9bfc17228b892672c6f
ZhanQi Ren <1292836261@qq.com>
2019-07-06 09:28:59 +0800

    moves 1000 times and then prints the resulting probability distribution.

---
+++ 定位/localization_1.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  5 10:12:02 2019
+
+@author: 12928
+"""
+p = [0,1,0,0,0]
+pHit = 0.6
+pMiss = 0.2
+pExact = 0.8
+pOvershoot = 0.1
+pUndershoot = 0.1
+
+world = ['green','red','red','green','green']
+measurements = ['red','green']
+def sense(p,Z):
+    q = []
+    for i in range(len(world)):
+        hit = (Z == world[i])
+        q.append(p[i]*(hit * pHit + (1-hit) * pMiss))
+    s = sum(q)
+    for i in range(len(q)):
+        q[i] = q[i] / s
+    return q
+#for k in range(len(measurements)):
+#    sense(p,measurements[k])
+    
+def move(p,U):
+    q = []
+    for i in range(len(p)):
+        s = p[(i - U) % len(p)] * pExact
+        s += p[(i - U -1) % len(p)] * pOvershoot
+        s += p[(i - U +1) % len(p)] * pUndershoot
+        q.append(s)
+    return q
+
+for i in range(1000):
+    p = move(p,1)
+print(p)
+
+
+        
+            
+            
+        
