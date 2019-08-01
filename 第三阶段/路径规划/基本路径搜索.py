f232a3f331526669ace72100ccc11c139a1968a0
ZhanQi Ren <1292836261@qq.com>
2019-08-01 23:14:47 +0800

    引入了A星算法，能够更加容易地找到最短路径

--- 第三阶段/路径规划/基本路径搜索.py
+++ 第三阶段/路径规划/基本路径搜索.py
@@ -1,11 +1,16 @@
 #grid format:
 # 0 = navigable space
 # 1 = occupied space
-grid = [[0,0,1,0,0,0],
-		[0,0,1,0,0,0],
-		[0,0,0,0,1,0],
-		[0,0,1,1,1,0],
-		[0,0,1,0,1,0]]
+grid = [[0,1,0,0,0,0],
+		[0,1,0,0,0,0],
+		[0,1,0,0,0,0],
+		[0,1,0,0,0,0],
+		[0,0,0,0,0,0]]
+heuristic = [[9,8,7,6,5,4],
+             [8,7,6,5,4,3],
+             [7,6,5,4,3,2],
+             [6,5,4,3,2,1],
+             [5,4,3,2,1,0]]
 init = [0,0]
 cost = 1
 goal = [len(grid)-1,len(grid[0])-1]
@@ -18,14 +23,18 @@ delta_name = ['^', '<', 'v', '>']
 def search(grid,init,goal,cost):
 	#openlistelementsareofthetype:[g,x,y]
     closed=[[0 for row in range(len(grid[0]))] for col in range(len(grid))]
+    expand=[[-1 for row in range(len(grid[0]))] for col in range(len(grid))]
     action=[[-1 for row in range(len(grid[0]))] for col in range(len(grid))]
     closed[init[0]][init[1]]=1
     x=init[0]
     y=init[1]
     g = 0
-    open = [[g,x,y]]
+    h = heuristic[x][y]
+    f = g + h
+    open = [[f,g,h,x,y]]
     found = False # flag that is set when search complete
     resign = False #flag set if we can't find expand
+    count = 0
     print ('initial open list:')
     for i in range(len(open)):
         print(' ',open[i])
@@ -45,9 +54,11 @@ def search(grid,init,goal,cost):
             next = open.pop()
             #print 'take list item'
             print(next)
-            x = next[1]
-            y = next[2]
-            g = next[0]
+            x = next[3]
+            y = next[4]
+            g = next[1]
+            expand[x][y] = count
+            count += 1
             #check if we are done
             if x == goal[0] and y == goal[1]:
                 found = True
@@ -62,7 +73,9 @@ def search(grid,init,goal,cost):
                     if x2 >= 0 and x2 < len(grid) and y2 >= 0 and y2 < len(grid[0]):
                         if closed[x2][y2] == 0 and grid[x2][y2] == 0:
                             g2 = g + cost
-                            open.append([g2,x2,y2])
+                            h2 = heuristic[x2][y2]
+                            f2 = g2 + h2
+                            open.append([f2,g2,h2,x2,y2])
                             print('append list item') 
                             print([g2,x2,y2])
                             print('-----------------')
@@ -78,6 +91,8 @@ def search(grid,init,goal,cost):
     x = goal[0]
     y = goal[1]
     policy[x][y] = '*'
+    for i in range(len(expand)):
+        print(expand[i])
     while x != init[0] or y != init[1]:
         x2 = x - delta[action[x][y]][0]
         y2 = y - delta[action[x][y]][1]
